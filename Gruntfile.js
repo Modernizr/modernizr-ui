7eaa8c1de5d39bc752a586089caf9206dbc2095d
Joe Critchley <joe@strobedigital.com>
2014-03-18 22:14:13 +0000

    Cleaned up gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,6 @@ module.exports = function(grunt) {
 
 	require("matchdep").filterAll("grunt-*").forEach(grunt.loadNpmTasks);
 
-	// var webpack = require("webpack");
-	// var webpackConfig = require("./webpack.config.js");
-
-
 	grunt.initConfig({
 		sass: {
 			app: {
@@ -20,51 +16,14 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		// browserify: {
-		// 	app: {
-		// 		options: {
-		// 			debug: true,
-		// 			transform: [require('grunt-react').browserify]
-		// 		},
-		// 		src: './src/js/main.js',
-		// 		dest: './build/app.js'
-		// 	}
-		// },
-		// 'webpack-dev-server': {
-		// 	options: {
-		// 		webpage: webpackConfig,
-		// 		publicPath: '/' + webpackConfig.output.publicPath
-		// 	},
-		// 	start: {
-		// 		keepAlive: true,
-		// 		webpack: {
-		// 			devtool: "eval",
-		// 			debug: true
-		// 		}
-		// 	}
-		// },
-		// copy: {
-		//   main: {
-		//     files: [
-		//       // includes files within path
-		//       {expand: true, src: ['./index.html'], dest: './build', filter: 'isFile'},
-		//     ]
-		//   }
-		// },
 		watch: {
 			sass: {
 				files: ['./src/css/*.scss'],
 				tasks: ['sass:app']
-			}//,
-			// app: {
-				// files: ['./src/js/**/*.js', './src/js/**/*.jsx'],
-				// tasks: ['webpack-watch']
-			// }
+			}
 		}
 	});
 
-	// grunt.registerTask('webpack', ['webpack-watch']);
 	grunt.registerTask('default', ['sass:app']);
-	// grunt.registerTask('start', 'webpack-dev-server:start');
 };
 
